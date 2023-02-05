b2fccb554966eecfb25433c09fb47e9578779744
Honza0105 <honzajelinek@email.cz>
2023-02-05 18:01:09 +0100

    restructure, creation of new windows

--- src/main/java/view/Assets/PlaneEditController.java
+++ src/main/java/view/Assets/PlaneEditController.java
@@ -1,4 +1,22 @@
 package view.Assets;
 
+import app.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class PlaneEditController {
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    private Label rentPerWeekLabel;
+
+    private Main main;
+
 }
--- src/main/java/view/Assets/YachtEditController.java
+++ src/main/java/view/Assets/YachtEditController.java
@@ -1,4 +1,40 @@
 package view.Assets;
 
+import app.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class YachtEditController {
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    private Label rentPerWeekLabel;
+
+    @FXML
+    private Label numberOfDecksLabel;
+
+    @FXML
+    private Label speedLabel;
+
+    @FXML
+    private Label hutsLabel;
+
+    @FXML
+    private Label lengthLabel;
+
+    private Main main;
+
+    @FXML
+    public void initilize(){
+    }
+
+
 }
