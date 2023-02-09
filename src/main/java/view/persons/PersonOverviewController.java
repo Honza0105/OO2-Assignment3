0bd7c0acd661298580aa8d6158902ea6184e8f2e
Honza0105 <honzajelinek@email.cz>
2023-02-09 17:34:12 +0100

    all fields defined

--- src/main/java/view/persons/PersonOverviewController.java
+++ src/main/java/view/persons/PersonOverviewController.java
@@ -1,4 +1,39 @@
 package view.persons;
 
+import domain.Asset;
+import domain.Heir;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class PersonOverviewController {
+
+    @FXML
+    private TableView<Heir> heirTableView;
+
+    @FXML
+    private TableColumn<Heir,String> nameColumn;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label addressLabel;
+
+    @FXML
+    private Label dateOfBirthLabel;
+
+    @FXML
+    private Label genderLabel;
+
+    @FXML
+    private Label netWorthLabel;
+
+    @FXML
+    private Label incomeLabel;
+
+    @FXML
+    private ListView<Asset> assetsListView;
 }
