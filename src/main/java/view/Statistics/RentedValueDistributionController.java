afb1548abc950c938188a393f6898d7a7d3284db
Honza0105 <honzajelinek@email.cz>
2023-02-12 13:07:39 +0100

    I think it works now :O

--- src/main/java/view/Statistics/RentedValueDistributionController.java
+++ src/main/java/view/Statistics/RentedValueDistributionController.java
@@ -4,50 +4,46 @@ import domain.Rent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 
-import java.math.BigDecimal;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 public class RentedValueDistributionController {
+
     @FXML
-    private ScatterChart<Number, Number> scatterChart;
+    private ScatterChart<String, Number> scatterChart;
 
-    private XYChart.Series<Number, Number> scatterChartData = new XYChart.Series<>();
-
-//    private NumberAxis xAxis;
+    @FXML
+    public void initialize() {
+//        XYChart.Series<String, Number> series = new XYChart.Series<>();
+//        series.setName("Distribution of Rented Value");
 //
-//    private NumberAxis yAxis;
-    public void setRentList(List<Rent> rentList) {
-        // X-axis: Rent duration in days
-        NumberAxis xaxis = new NumberAxis();
-        xaxis.setLabel("Duration (days)");
+//        ObservableList<XYChart.Data<String, Number>> data = FXCollections.observableArrayList();
+//        data.add(new XYChart.Data<>("Day 1", 500));
+//        data.add(new XYChart.Data<>("Day 2", 400));
+//        data.add(new XYChart.Data<>("Day 3", 600));
+//        data.add(new XYChart.Data<>("Day 4", 450));
+//        data.add(new XYChart.Data<>("Day 5", 700));
+//        series.setData(data);
+//
+//        scatterChart.getData().add(series);
+    }
 
-        // Y-axis: Rent amount in euros
-        NumberAxis yaxis = new NumberAxis();
-        yaxis.setLabel("Rent Amount (€)");
+    public void setRentList(ObservableList<Rent> rentList){
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Distribution of Rented Value");
 
-        // Configure ScatterChart
-        scatterChart = new ScatterChart<>(xaxis, yaxis);
-        scatterChart.setTitle("Rent Duration vs Rent Amount");
+        ObservableList<XYChart.Data<String, Number>> data = FXCollections.observableArrayList();
 
-        // Configure Series and adding data to the series
-        XYChart.Series<Number, Number> series = new XYChart.Series<>();
         for (Rent rent : rentList) {
-            long duration = ChronoUnit.DAYS.between(rent.getStart(), rent.getEnd());
-            series.getData().add(new XYChart.Data<>(duration, rent.getRent().doubleValue()));
-            System.out.println(series.getData());
+            long daysBetween = ChronoUnit.DAYS.between(rent.getStart(), rent.getEnd());
+            data.add(new XYChart.Data<>(String.format("Day %d", daysBetween), rent.getRent().doubleValue()));
         }
 
-        // Adding series to the ScatterChart
+        series.setData(data);
+
         scatterChart.getData().add(series);
-        System.out.println(rentList);
-        System.out.println(series);
     }
 
 }
-
-
